75df982b86e2a4ebc54211a84931a429d569ed6a
rkukutis <kukutis.ricardas@gmail.com>
2023-11-28 20:38:18 +0200

    started stuff

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+type ThingieProps = {
+  thingies: string[];
+};
+
+const stuff: string[] = ["Hello", "There", "Heh"];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text text-red-600 text-center py-8 font-extrabold">
+        BRUH
+      </h1>
+      <ThingieDisplay thingies={stuff} />
     </>
-  )
+  );
 }
 
-export default App
+function ThingieDisplay({ thingies }: ThingieProps) {
+  return thingies.map((t) => <h2>{t}</h2>);
+}
+
+export default App;
