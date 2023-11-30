5b207b3706d08e7f4709ef56160fc55c22b014a0
rkukutis <kukutis.ricardas@gmail.com>
2023-11-30 12:09:40 +0200

    Fixed image import

--- playground/src/pages/Home.tsx
+++ playground/src/pages/Home.tsx
@@ -1,10 +1,12 @@
+import cat from "../assets/cat.jpg";
+
 export default function Home() {
   return (
     <section className=" bg-slate-200 h-screen flex flex-col justify-center">
       <div className="flex items-center flex-col space-y-2">
         <h1>Welcome to my website!</h1>
         <h3>There's nothing here yet, but have a picture of this cute kitty :)</h3>
-        <img className="w-[20rem] py-5" src="../../public/cat.jpg" />
+        <img className="w-[20rem] py-5" src={cat} />
         {/* <List /> */}
       </div>
     </section>
