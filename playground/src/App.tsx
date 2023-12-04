2c8489d6e28e78a2adf24b3a504c2b86a69dbfbc
rkukutis <kukutis.ricardas@gmail.com>
2023-12-04 21:42:05 +0200

    Added font

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
