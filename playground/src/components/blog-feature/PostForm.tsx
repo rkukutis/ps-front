f38d5c7e540bff55caf1cb50475e73db37b1d278
rkukutis <kukutis.ricardas@gmail.com>
2023-12-08 19:21:03 +0200

    Removed unused import

--- playground/src/components/blog-feature/PostForm.tsx
+++ playground/src/components/blog-feature/PostForm.tsx
@@ -9,7 +9,6 @@ import StarterKit from "@tiptap/starter-kit";
 import Image from "@tiptap/extension-image";
 import MenuBar from "../editor-feature/MenuBar";
 import { Color } from "@tiptap/extension-color";
-import ListItem from "@tiptap/extension-list-item";
 import TextStyle from "@tiptap/extension-text-style";
 import Underline from "@tiptap/extension-underline";
 
